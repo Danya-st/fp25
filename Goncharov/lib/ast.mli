65a145e42932bb0df00fd86a9e2f683c1101983a
danya <godami2006@gmail.com>
2026-03-21 15:35:31 +0300

    =fixed lints

--- Goncharov/lib/ast.mli
+++ Goncharov/lib/ast.mli
@@ -3,17 +3,20 @@
 (** Copyright 2021-2024, Kakadu and contributors *)
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+[@@@ocaml.text "/*"]
+
 type binop =
-  | Add
-  | Sub
-  | Mul
-  | Div
-  | Eq
-  | Leq
-  | Geq
-  | Lt
-  | Gt
-  | Neq
+  | Add (** + **)
+  | Sub (** - **)
+  | Mul (** * **)
+  | Div (** / **)
+  | Eq (** = **)
+  | Leq (** <= **)
+  | Geq (** >= **)
+  | Lt (** < **)
+  | Gt (** > **)
+  | Neq (** <> **)
 
 [@@@ocaml.text "/*"]
 
@@ -24,9 +27,9 @@ type 'name t =
   | Var of 'name (** Variable [x] *)
   | Fun of 'name * 'name t (** fun **)
   | App of 'name t * 'name t (** Application [f g] *)
-  | Int of int
-  | Neg of 'name t
-  | Bin of binop * 'name t * 'name t
-  | If of 'name t * 'name t * 'name t
-  | Let of 'name * 'name t * 'name t
-  | LetRec of 'name * 'name t * 'name t
+  | Int of int (** Int **)
+  | Neg of 'name t (** Negative int -x **)
+  | Bin of binop * 'name t * 'name t (** binary operation x * y **)
+  | If of 'name t * 'name t * 'name t (** if then else **)
+  | Let of 'name * 'name t * 'name t (** let x = body in e **)
+  | LetRec of 'name * 'name t * 'name t (** let rec x = body in e **)
